4ccdac845a80fa39c913a7506bc0c74679083447
Trần Ngọc Quang <56824077+tranngoc769@users.noreply.github.com>
2020-03-11 12:19:13 +0700

    install module and route index page

--- index.js
+++ index.js
@@ -1,24 +1,63 @@
+/* DECLASRE */
 var express = require('express');
 var path = require('path');
 var db = require('mysql');
 var app = express();
 const connect = require('./utils/db')
-// set up view engine
+var bodyParser = require("body-parser");
+var session = require('express-session');
+var validator = require('express-validator');
+/* @@END DECLASRE */
+
+/* CONNECT DATABASE */
 // connect.connect();
+/* @@END CONNECT DATABASE */
+
+/* SESSION */
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}))
+/* @@END SESSION */
+
+/* MESSAGES */
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+/* @@END MESSAGES */
+
+// VIEWS ENGINE
 app.set('views', path.join(__dirname,'views'));
 app.set('view engine','ejs');
-
 app.use(express.static(path.join(__dirname,'public')));
-//
-app.get('/',(req,res)=>
-{
-    res.render('index',
-    {
-        Title : 'Trang Chủ'
-    });
-})
-var port = 3000;
+/* @@END VIEWS ENGINE */
 
+/* BODY_PARSER */
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+/* END @@BODY_PARSER */
+
+/* VALIDATOR */
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+/* END @@VALIDATOR */
+
+/* ROUTE */
+var router = require('./routes/route');
+app.use('/', router);
+var adminrouter = require('./routes/adminroute');
+app.use('/admin', adminrouter);
+/* @@END ROUTE */
+
+
+
+// START SERVER
+var port = 3000;
 app.listen(port,()=>
 {
     console.log("Server is running");
